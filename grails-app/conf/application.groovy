c4bb5918dcc8ecb9c7ab887c3c8723e74f581951
Manuel <jose.delvillar@ulsaoaxaca.edu.mx>
2020-01-24 09:42:46 -0600

    401 error on not logged

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -3,19 +3,15 @@ grails.plugin.springsecurity.userLookup.userDomainClassName = 'com.mysecurerest.
 grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'com.mysecurerest.UserAuthority'
 grails.plugin.springsecurity.authority.className = 'com.mysecurerest.Authority'
 grails.plugin.springsecurity.controllerAnnotations.staticRules = [
-		[pattern: '/',               access: ['permitAll']],
-		[pattern: '/error',          access: ['permitAll']],
-		[pattern: '/index',          access: ['permitAll']],
-		[pattern: '/index.gsp',      access: ['permitAll']],
-		[pattern: '/shutdown',       access: ['permitAll']],
-		[pattern: '/assets/**',      access: ['permitAll']],
-		[pattern: '/**/js/**',       access: ['permitAll']],
-		[pattern: '/**/css/**',      access: ['permitAll']],
-		[pattern: '/**/images/**',   access: ['permitAll']],
+		[pattern: '/assets/**', access: ['permitAll']],
+		[pattern: '/**/js/**', access: ['permitAll']],
+		[pattern: '/**/css/**', access: ['permitAll']],
+		[pattern: '/**/images/**', access: ['permitAll']],
 		[pattern: '/**/favicon.ico', access: ['permitAll']],
-		[pattern: '/notAuthorized',  access: ['permitAll']],
-		[pattern: '/api',			 access: ['permitAll']],
-		[pattern: '/api/**', 		 access: ['ROLE_USER']]
+		[pattern: '/', access: ['permitAll']],
+		[pattern: '/api', access: ['permitAll']],
+		[pattern: '/notAuthorized', access: ['permitAll']],
+		[pattern: '/**', access: ['ROLE_USER']]
 ]
 
 
